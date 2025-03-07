4867b2bef2f3bcb28485be165ce5bccba9e955ea
Santiago Lopez Marmolejo <santiagolopezmarmolejo2017@gmail.com>
2025-03-07 11:55:53 -0500

    Fixes

--- src/modules/user/infrastructure/service/user.service.ts
+++ src/modules/user/infrastructure/service/user.service.ts
@@ -1,17 +1,18 @@
-import { Injectable, NotFoundException, RequestTimeoutException } from "@nestjs/common";
+import { ConflictException, HttpStatus, Injectable, NotFoundException, RequestTimeoutException } from "@nestjs/common";
 import { IUserService } from "../../domain/service/IUser.service";
 import { IUser, IUserCreate, IUserUpdate } from "../../domain/interfaces/IUser";
 import { InjectRepository } from "@nestjs/typeorm";
 import { User } from "../entity/user.entity";
 import { Repository } from "typeorm";
 import { JwtService } from "@nestjs/jwt";
+import { Auth } from "src/modules/auth/infractructure/entity/auth.entity";
 
 @Injectable()
 export class UserService implements IUserService {
       constructor(
         @InjectRepository(User) 
         private readonly userRepository: Repository<User>,
-        private readonly jwtService: JwtService, // 🔹 Inyectamos correctamente el JwtService
+        private readonly jwtService: JwtService, 
       ) {}
 
       
@@ -40,16 +41,29 @@ export class UserService implements IUserService {
     async create(userDto: IUserCreate): Promise<IUser> {
       let user: User | undefined;
       console.log(user)
+
+      const existingUser = await this.userRepository.findOne({
+        where: { auth: { email: userDto.auth.email } },
+        relations: ['auth'],
+      });
+
+      if (existingUser) {
+        throw new ConflictException('El usuario con este correo ya está registrado.');
+      }
+
       try {
         user = this.userRepository.create(userDto);
       } catch (error) {
+        
         throw new RequestTimeoutException('Cannot create user', {
           description: 'Error creating user',
         });
-      }
+      }  
+
       const token = this.jwtService.sign({ id: user.id, email: user.auth.email });
       console.log(token)
       await this.userRepository.save(user);
+      
     return user;
    }
 
